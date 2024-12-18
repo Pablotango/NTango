5bb8b955b1e3292a72aa4f4649b816672e8c3596
Pablo <92504860+Pablotango@users.noreply.github.com>
2024-12-18 19:52:25 +0930

    NTango.py

--- NTango.py
+++ NTango.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.write(st.secrets)  # This will display all secrets in the Streamlit app
 
+credentials_json = json.loads(st.secrets["google"]["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
